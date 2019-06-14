a18aef14bd45e9a2276fb1c8531f33ee3bbf1d68
tany-spi5 <tany-spi@yandex.ru>
2019-06-14 19:25:45 +0400

    2-hw1

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,23 +1,21 @@
 public class Variable {
-public static void main (String[] args)	{
-	byte KolYader=1;
-	short Kol1=32000;
-    int Kol2=1000000;
-    Long Kol3=20000000000L;
-    float prozessor=2.67f;
-    double Kol4=0.000045158458;
-    char Kol5='Z';
-    boolean Check;
-    Check=true;
-    System.out.println("KolYader=" + KolYader);
-    System.out.println("Kol1=" + Kol1);
-    System.out.println("Kol2=" + Kol2);
-    System.out.println("Kol3=" + Kol3);
-    System.out.println("prozessor=" + prozessor);
-    System.out.println("Kol4=" + String.format("%.12f",Kol4));
-    System.out.println("Kol5=" + Kol5);	
-    System.out.println("Check=" + Check);
-}
-
-
+       public static void main (String[] args)	{
+	          byte KolYader=1;
+	          short Kol1=32000;
+              int Kol2=1000000;
+              Long Kol3=20000000000L;
+              float prozessor=2.67f;
+              double Kol4=0.000045158458;
+              char Kol5='Z';
+              boolean Check;
+                      Check=true;
+                           System.out.println("KolYader=" + KolYader);
+                           System.out.println("Kol1=" + Kol1);
+                           System.out.println("Kol2=" + Kol2);
+                           System.out.println("Kol3=" + Kol3);
+                           System.out.println("prozessor=" + prozessor);
+                           System.out.println("Kol4=" + String.format("%.12f",Kol4));
+                           System.out.println("Kol5=" + Kol5);	
+                           System.out.println("Check=" + Check);
+          }
 }
