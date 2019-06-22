c66e1af55cbbe45fe814fe2d1c9dda4023acdce5
tany-spi5 <tany-spi@yandex.ru>
2019-06-22 20:29:14 +0400

    8_1 HW1

--- Lesson_1/Cycle.java
+++ Lesson_1/Cycle.java
@@ -1,26 +1,26 @@
-public  class Cycle {
+public class Cycle {
 	public static void main(String[] args) {
-		for(int i=0;i <= 20;i++) {
-			System.out.print(i+" ");
+		for(int i = 0; i <= 20; i++) {
+			System.out.print(i + " ");
 		}
 		System.out.println(" ");
-		int k=6;
+		int k = 6;
 		while(k >= -6) {
-				System.out.print(k+" ");
-				k=k-2;
+			System.out.print(k + " ");
+			k -= 2;
 		}
 		System.out.println(" ");
-		int h=10;
-		int sum=0;
+		int h = 10;
+		int sum = 0;
 		do{
 			if (h % 2 == 0) {
+			//	System.out.println(h + " even");
 			}	else {
-					System.out.println(h+" odd");
-					sum=sum+h;
+				System.out.println(h + " odd");
+				sum += h;
 				}			
-			h=h+1;
-		}
-		while (h<=20);
-		System.out.println(sum + " odd ");
+			h++;
+		}	while (h <= 20);
+		System.out.println(sum + " sum odd ");
 	}
  }
--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,21 +1,20 @@
 public class Variable {
-       public static void main (String[] args)	{
-	          byte KolYader=1;
-	          short Kol1=32000;
-              int Kol2=1000000;
-              Long Kol3=20000000000L;
-              float prozessor=2.67f;
-              double Kol4=0.000045158458;
-              char Kol5='Z';
-              boolean Check;
-                      Check=true;
-                           System.out.println("KolYader=" + KolYader);
-                           System.out.println("Kol1=" + Kol1);
-                           System.out.println("Kol2=" + Kol2);
-                           System.out.println("Kol3=" + Kol3);
-                           System.out.println("prozessor=" + prozessor);
-                           System.out.println("Kol4=" + String.format("%.12f",Kol4));
-                           System.out.println("Kol5=" + Kol5);	
-                           System.out.println("Check=" + Check);
-          }
+    public static void main (String[] args)	{
+	    byte quantityYader = 1;
+        short quantity1 = 32000;
+        int quantity2 = 1000000;
+        long quantity3 = 20000000000L;
+        float prozessor = 2.67f;
+        double quantity4 = 0.000045158458;
+        char letter = 'Z';
+        boolean сheck = true;
+            System.out.println("quantityYader=" + quantityYader);
+            System.out.println("quantity1=" + quantity1);
+            System.out.println("quantity2=" + quantity2);
+            System.out.println("quantity3=" + quantity3);
+            System.out.println("prozessor=" + prozessor);
+            System.out.println("quantity4=" + String.format("%.12f",quantity4));
+            System.out.println("letter=" + letter);	
+            System.out.println("check=" + сheck);
+    }
 }
