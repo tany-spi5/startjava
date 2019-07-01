aa745343cdc01fda6880f94767e101462a6d8f8a
tany-spi5 <tany-spi@yandex.ru>
2019-07-01 20:09:43 +0400

    9_3 hw

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,33 +1,32 @@
-import java.util.Scanner;
-class ConditionalStatement {
+Public class ConditionalStatement {
     public static void main (String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Your name begin with letter ");
-        String letter = scan.nextLine();
+        String letter = "T";
         System.out.println(letter);
-        int yourAge = 40;
-        if (yourAge > 20) {
+        int Age = 40;
+        if (Age > 20) {
             System.out.println("My age > 20");
         }
+        System.out.println();
         boolean isMan = false;
-        if (isMan == true) {
+        if (isMan) {
             System.out.println("I am a man");
         }
-        if (!(isMan) == true) {
+        if (!(isMan)) {
             System.out.println("I am a woman");
         }
+        System.out.println();
         float growth = 1.64f;
         if (growth < 1.80) {
             System.out.println ("My growth < 1.80");
         } else {
             System.out.println ("My growth > 1.80");
         }
+        System.out.println();
         if (letter == "M") {
             System.out.println ("my name begin with M");
         } else if (letter == "I") {
             System.out.println ("my name begin with I");
-        }
-        else {
+        } else {
             System.out.println ("my name begin with " + letter);
         }
     }
--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,21 +1,21 @@
 public class Variable {
     public static void main (String[] args) {
-    byte pen = 1;
-    short pea = 32000;
-    int pencil = 1000000;
-    long penny = 20000000000L;
-    float speed = 2.67f;
-    double quantity = 0.000045158458;
-    char letter = 'Z';
-    boolean сheck = true;
+        byte cores = 1;
+        short processor = 32000;
+        int disk = 1000000;
+        long cooler = 20000000000L;
+        float speed = 2.67f;
+        double quantity = 0.000045158458;
+        char letter = 'Z';
+        boolean сheck = true;
 
-    System.out.println("pen = " + pen);
-    System.out.println("pea = " + pea);
-    System.out.println("pencil = " + pencil);
-    System.out.println("penny = " + penny);
-    System.out.println("speed = " + speed);
-    System.out.println("quantity = " + String.format("%.12f",quantity));
-    System.out.println("letter = " + letter);	
-    System.out.println("check  = " + сheck);
+        System.out.println("cores = " + cores);
+        System.out.println("processor = " + processor);
+        System.out.println("disk = " + disk);
+        System.out.println("cooler= " + cooler);
+        System.out.println("speed = " + speed);
+        System.out.println("quantity = " + String.format("%.12f",quantity));
+        System.out.println("letter = " + letter);	
+        System.out.println("check = " + сheck);
     }
 }
