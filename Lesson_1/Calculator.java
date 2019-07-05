b3c5b6561e607db725a0e4c08b4f7ddc83033e40
tany-spi5 <tany-spi@yandex.ru>
2019-07-05 16:48:29 +0400

    9_7 hw

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -6,11 +6,10 @@ public class Calculator {
         System.out.println("number2 = " + number2);
         String operation = "+";
         //System.out.println("operation = " + operation);
-        int sum = 0;
-        int result=1;
+        int result = 0;
         if (operation == "+") {
-            sum = number1 + number2;
-            System.out.println("+ sum = " + sum);
+            result = number1 + number2;
+            System.out.println("+ result = " + result);
         } else if (operation == "-") {
             result = number1 - number2;
             System.out.println("-  result = " + result);
@@ -21,133 +20,9 @@ public class Calculator {
             result = number1 / number2;
             System.out.println("/  result = " + result);
         } else if (operation == "^") {
-            for(int i=1; i<=number2; i++) {
-                result=result*number1;
-            }
-            System.out.println("^  result = " + result);
-        } else if (operation == "%") {
-            result = number1 % number2;
-            System.out.println("%  result = " + result);
-        } else {
-            System.out.println("operation = " + operation);
-        }
-        operation = "-";
-        result=1;
-        if (operation == "+") {
-            sum = number1 + number2;
-            System.out.println("+ sum = " + sum);
-        } else if (operation == "-") {
-            result = number1 - number2;
-            System.out.println("-  result = " + result);
-        } else if (operation == "*") {
-            result = number1 * number2;
-            System.out.println("*  result = " + result);
-        } else if (operation == "/") {
-            result = number1 / number2;
-            System.out.println("/  result = " + result);
-        } else if (operation == "^") {
-            for(int i=1; i<=number2; i++) {
-                result=result*number1;
-            }
-            System.out.println("^  result = " + result);
-        } else if (operation == "%") {
-            result = number1 % number2;
-            System.out.println("%  result = " + result);
-        } else {
-            System.out.println("operation = " + operation);
-        }
-        operation = "*";
-        result=1;
-        if (operation == "+") {
-            sum = number1 + number2;
-            System.out.println("+ sum = " + sum);
-        } else if (operation == "-") {
-            result = number1 - number2;
-            System.out.println("-  result = " + result);
-        } else if (operation == "*") {
-            result = number1 * number2;
-            System.out.println("*  result = " + result);
-        } else if (operation == "/") {
-            result = number1 / number2;
-            System.out.println("/  result = " + result);
-        } else if (operation == "^") {
-            for(int i=1; i<=number2; i++) {
-                result=result*number1;
-            }
-            System.out.println("^  result = " + result);
-        } else if (operation == "%") {
-            result = number1 % number2;
-            System.out.println("%  result = " + result);
-        } else {
-            System.out.println("operation = " + operation);
-        }
-        operation = "/";
-        result=1;
-        if (operation == "+") {
-            sum = number1 + number2;
-            System.out.println("+ sum = " + sum);
-        } else if (operation == "-") {
-            result = number1 - number2;
-            System.out.println("-  result = " + result);
-        } else if (operation == "*") {
-            result = number1 * number2;
-            System.out.println("*  result = " + result);
-        } else if (operation == "/") {
-            result = number1 / number2;
-            System.out.println("/  result = " + result);
-        } else if (operation == "^") {
-            for(int i=1; i<=number2; i++) {
-                result=result*number1;
-            }
-            System.out.println("^  result = " + result);
-        } else if (operation == "%") {
-            result = number1 % number2;
-            System.out.println("%  result = " + result);
-        } else {
-            System.out.println("operation = " + operation);
-        }
-        operation = "^";
-        result=1;
-        if (operation == "+") {
-            sum = number1 + number2;
-            System.out.println("+ sum = " + sum);
-        } else if (operation == "-") {
-            result = number1 - number2;
-            System.out.println("-  result = " + result);
-        } else if (operation == "*") {
-            result = number1 * number2;
-            System.out.println("*  result = " + result);
-        } else if (operation == "/") {
-            result = number1 / number2;
-            System.out.println("/  result = " + result);
-        } else if (operation == "^") {
-            for(int i=1; i<=number2; i++) {
-                result=result*number1;
-            }
-            System.out.println("^  result = " + result);
-        } else if (operation == "%") {
-            result = number1 % number2;
-            System.out.println("%  result = " + result);
-        } else {
-            System.out.println("operation = " + operation);
-        }
-        operation = "%";
-        result=1;
-        if (operation == "+") {
-            sum = number1 + number2;
-            System.out.println("+ sum = " + sum);
-        } else if (operation == "-") {
-            result = number1 - number2;
-            System.out.println("-  result = " + result);
-        } else if (operation == "*") {
-            result = number1 * number2;
-            System.out.println("*  result = " + result);
-        } else if (operation == "/") {
-            result = number1 / number2;
-            System.out.println("/  result = " + result);
-        } else if (operation == "^") {
-            for(int i=1; i<=number2; i++) {
-                result=result*number1;
+            result = 1; 
+            for(int i = 1; i <= number2; i++) {
+                result = result * number1;
             }
             System.out.println("^  result = " + result);
         } else if (operation == "%") {
