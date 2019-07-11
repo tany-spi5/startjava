b8c4e68ff44b19bd417dc538850335706748f545
tany-spi5 <tany-spi@yandex.ru>
2019-07-11 19:43:43 +0400

    9_13 hw

--- Lesson_1/Game.java
+++ Lesson_1/Game.java
@@ -1,41 +1,13 @@
 public class Game {
     public static void main(String[] args) {
         int puzzle_number = 45;
-        int i;  // число от 1 до 100
+        int i;  // число от 1 до 1000
         System.out.println();
-
-        if (puzzle_number >= 50) {
-            if (puzzle_number >= 75) {
-                for (i = 75; i <= 100; i++) {
+                for (i = 1; i <= 1000; i++) {
                     if (puzzle_number == i) {
                         System.out.println("find number =" + i);
                         break;
                     }
                 }
-            } else {
-                for (i = 74; i >= 50; i--) {
-                    if (puzzle_number == i) {
-                        System.out.println("find number =" + i);
-                        break;
-                    }
-                }
-            }
-        } else {
-            if (puzzle_number >= 25) {
-                for (i = 25; i <= 49; i++) {
-                    if (puzzle_number == i) {
-                        System.out.println("find number =" + i);
-                        break;
-                    }
-                }
-            } else {
-                for (i = 1; i <= 24; i++) {
-                    if (puzzle_number == i) {
-                        System.out.println("find number =" + i);
-                        break;
-                    }
-                }
-            }
-        }
     }
 }
--- Lesson_1/Unicode.java
+++ Lesson_1/Unicode.java
@@ -1,7 +1,6 @@
 public class Unicode {
     public static void main(String[] args) {
-        char i;
-        for (i = 33; i <= 126; i++) {
+        for ( char i = 33; i <= 126; i++) {
             System.out.print(i);
         }
         System.out.println();
