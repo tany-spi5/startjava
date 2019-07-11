06379893734ff8659e295f6438738a83ad0deb66
tany-spi5 <tany-spi@yandex.ru>
2019-07-11 19:34:49 +0400

    9_14 hw

--- Lesson_1/Game.java
+++ Lesson_1/Game.java
@@ -1,13 +1,15 @@
 public class Game {
     public static void main(String[] args) {
-        int puzzle_number = 45;
+        int puzzleNumber = 45;
         int i;  // число от 1 до 1000
         System.out.println();
-                for (i = 1; i <= 1000; i++) {
-                    if (puzzle_number == i) {
-                        System.out.println("find number =" + i);
-                        break;
-                    }
+            for (i = 1; i <= 1000; i++) {
+                if (puzzleNumber == i) {
+                    System.out.println("find number =" + i);
+                    break;
+                } else {
+                	System.out.println("introduce number < puzzle number   " +  i);
                 }
+            }
     }
 }
