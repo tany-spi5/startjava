bf66862bd074fa13868ea9b815236c1f65988991
tany-spi5 <tany-spi@yandex.ru>
2019-07-11 19:07:42 +0400

    9_12 hw

--- Lesson_1/Game.java
+++ Lesson_1/Game.java
@@ -1,36 +1,36 @@
 public class Game {
-    public static void main (String[] args) {
-        int number = 45;
-        int i;
+    public static void main(String[] args) {
+        int puzzle_number = 45;
+        int i;  // число от 1 до 100
         System.out.println();
 
-        if (number >= 50) {
-            if (number >= 75) {
+        if (puzzle_number >= 50) {
+            if (puzzle_number >= 75) {
                 for (i = 75; i <= 100; i++) {
-                    if (number == i) {
+                    if (puzzle_number == i) {
                         System.out.println("find number =" + i);
                         break;
                     }
                 }
             } else {
                 for (i = 74; i >= 50; i--) {
-                    if (number == i) {
+                    if (puzzle_number == i) {
                         System.out.println("find number =" + i);
                         break;
                     }
                 }
             }
         } else {
-            if (number >= 25) {
+            if (puzzle_number >= 25) {
                 for (i = 25; i <= 49; i++) {
-                    if (number == i) {
+                    if (puzzle_number == i) {
                         System.out.println("find number =" + i);
                         break;
                     }
                 }
             } else {
                 for (i = 1; i <= 24; i++) {
-                    if (number == i) {
+                    if (puzzle_number == i) {
                         System.out.println("find number =" + i);
                         break;
                     }
---
+++ Lesson_1/Unicode.java
@@ -0,0 +1,9 @@
+public class Unicode {
+    public static void main(String[] args) {
+        char i;
+        for (i = 33; i <= 126; i++) {
+            System.out.print(i);
+        }
+        System.out.println();
+    }
+}
