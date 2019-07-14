6878455a45222093803601305c8b5876f232be55
tany-spi5 <tany-spi@yandex.ru>
2019-07-14 19:22:17 +0400

    2_2_1 hw

---
+++ Lesson_2/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+    char floor='w';
+    String Name="Tanya";
+    float height=164.5f;
+    float weight=55.7f;
+    int age=30;
+    void move() {
+        System.out.println("you went 5 steps");
+    }
+    void sit() {
+        System.out.println("you sat down");
+    }
+    void run() {
+        System.out.println("you ran 5 steps");
+    }
+    void say() {
+    
+    }
+    void learnJava() {
+        System.out.println("you learn Java");
+    }
+}
