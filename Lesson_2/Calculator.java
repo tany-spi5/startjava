a10e6c78b007dad9af9b2118cf427952d93a072a
tany-spi5 <tany-spi@yandex.ru>
2019-07-22 21:30:59 +0400

    2_6_2 hw

--- Lesson_2/Calculator.java
+++ Lesson_2/Calculator.java
@@ -1,7 +1,7 @@
 public class Calculator {
-    private int number1;
-    private int number2;
-    private String operation;
+    private int number1 = 123;
+    private int number2 = 3;
+    private String operation = "+";
 
     public int getNumber1() {
         return number1;
@@ -38,4 +38,31 @@ public class Calculator {
             this.operation = operation;
         }
     }
+
+        switch (operation) {
+        case "+":
+            System.out.println("sum result = " + ( number1 + number2));
+            break;
+        case "-":
+            System.out.println("difference result = " + ( number1 - number2));
+            break;
+        case "*":
+            System.out.println("increase result = " + ( number1 * number2));
+            break;
+        case "/":
+            System.out.println("division result = " + ( number1 / number2));
+            break;
+        case "^": int result = 1;
+            for(int i = 1; i <= number2; i++) {
+                result *= number1;
+            }
+            System.out.println("degree  result = " + result);
+            break;
+        case "%": 
+            System.out.println("%  result = " + ( number1 % number2));
+            break; 
+        default:
+            System.out.println("unknown operation = " + operation);
+            break;
+        }
 } 
--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -9,19 +9,5 @@ public class CalculatorTest {
         System.out.println("First number = " + mycalc.getNumber1());
         System.out.println("Operation = " + mycalc.getOperation());
         System.out.println("Second number = " + mycalc.getNumber2());
-
-        switch(mycalc.getOperation()) {
-        case "+": System.out.println("sum result = " + ( mycalc.getNumber1() + mycalc.getNumber2())); break;
-        case "-": System.out.println("difference result = " + ( mycalc.getNumber1() - mycalc.getNumber2())); break;
-        case "*": System.out.println("increase result = " + ( mycalc.getNumber1() * mycalc.getNumber2())); break;
-        case "/": System.out.println("division result = " + ( mycalc.getNumber1() / mycalc.getNumber2())); break;
-        case "^": int result = 1;
-            for(int i = 1; i <= mycalc.getNumber2(); i++) {
-                result *= mycalc.getNumber1();
-            }
-            System.out.println("degree  result = " + result); break;
-        case "%": System.out.println("%  result = " + ( mycalc.getNumber1() % mycalc.getNumber2())); break; 
-        default: System.out.println("unknown operation = " + mycalc.getOperation()); break;
-        }
     }
 }
