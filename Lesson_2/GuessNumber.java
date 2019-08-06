c89f17d4187370638b2c1e98d51296dab62715ef
tany-spi5 <tany-spi@yandex.ru>
2019-08-06 22:38:32 +0400

    2_6_19 hw

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -1,9 +1,13 @@
 public class GuessNumber {
     private int puzzleNumber;
     private int playerNumber;  // число от 1 до 100
-    public GuessNumber(int puzzleNumber1, int playerNumber1) {
+    private String player1;
+    private String player2;
+    public GuessNumber(int puzzleNumber1, int playerNumber1, String player1, String player2) {
         puzzleNumber = puzzleNumber1;
         playerNumber = playerNumber1;
+        this.player1=player1;
+        this.player2=player2;
     }
     public int getPuzzleNumber() {
         return puzzleNumber;
@@ -36,12 +40,39 @@ public class GuessNumber {
             System.out.println("playerNumber =" + playerNumber);
         }       
     }
-    
+
      public void verification() {
         if (puzzleNumber == playerNumber) {
             System.out.println("find number =" + playerNumber);
         } 
     }     
     
+    public void play() {
+
+    
+        playerNumber = 1;
+        System.out.println(player1);
+        System.out.println(playerNumber);
+        verification();
+        System.out.println(player2);
+        game();
+        verification();
+        do {
+            System.out.println(player1);
+            game();
+            verification();
+            if (puzzleNumber == playerNumber) {
+                break;
+            }
+            System.out.println(player2);
+            game();
+            verification();
+            if (puzzleNumber == playerNumber) {
+                break;
+            }
+        }
+        while (puzzleNumber != playerNumber);
+
+    }
 }
 
--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -5,33 +5,36 @@ public class GuessNumberTest {
     public static void main(String[] args) {
         int puzzleNumber1;
         int playerNumber1;
+        String continuation;
         Scanner scan = new Scanner(System.in); 
-        System.out.println("Enter the first player");
-        String player1 =scan.next();
-        System.out.println("Enter the second player");
-        String player2 =scan.next();
-        Random random = new Random();
-        puzzleNumber1 = random.nextInt(100);
-        System.out.println("puzzleNumber = " + puzzleNumber1);
-        Player  peter = new Player(player1, 25);
-        playerNumber1 = 1;
-        GuessNumber  myNumber = new GuessNumber(puzzleNumber1, playerNumber1);
-        System.out.println(peter.getName());
-        System.out.println(myNumber.getPlayerNumber());
-        myNumber.verification();
-        Player  john = new Player(player2, 30);
-        System.out.println(john.getName());
-        myNumber.game();
-        myNumber.verification();
         do {
-            System.out.println(peter.getName());
-            myNumber.game();
-            myNumber.verification();
-            System.out.println(john.getName());
-            myNumber.game();
-            myNumber.verification();
-            
-        }
-        while (myNumber.getPuzzleNumber() != myNumber.getPlayerNumber());
+            System.out.println("Enter the first player");
+            String player1 =scan.next();
+            System.out.println("Enter the second player");
+            String player2 =scan.next();
+            Random random = new Random();
+            puzzleNumber1 = random.nextInt(100);
+            System.out.println("puzzleNumber = " + puzzleNumber1);
+            Player  peter = new Player(player1, 25);
+            Player  john = new Player(player2, 30);
+            playerNumber1 = 1;
+            GuessNumber  myNumber = new GuessNumber(puzzleNumber1, playerNumber1, player1, player2);
+            myNumber.play();
+            do {
+                System.out.println("I would like continue yes/no");
+                continuation = scan.nextLine();
+                if (continuation.equals("yes")) {
+                    System.out.println("continuation = " + continuation);
+                    break;
+                } else if (continuation.equals("no")) {
+                    break;
+                } else {
+                    System.out.println("continuation = " + continuation + "  you must enter yes or no");
+                }           
+            } while ((continuation != "yes") & (continuation != "no"));
+
+        } while (continuation.equals("yes"));
+
+
     }
 }
